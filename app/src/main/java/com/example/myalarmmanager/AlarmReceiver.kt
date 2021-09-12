f0574f08d2fd81f75e2acbccf6910aa5a00f0faf
Asep Sutisna <sutisnaasep323@gmail.com>
2021-09-12 23:23:12 +0700

    Mengatur Alarm Manager

--- app/src/main/java/com/example/myalarmmanager/AlarmReceiver.kt
+++ app/src/main/java/com/example/myalarmmanager/AlarmReceiver.kt
@@ -1,9 +1,15 @@
 package com.example.myalarmmanager
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.Toast
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AlarmReceiver : BroadcastReceiver() {
 
@@ -16,6 +22,9 @@ class AlarmReceiver : BroadcastReceiver() {
         // siapkan 2 id untuk 2 macam alarm
         private const val ID_ONETIME = 100
         private const val ID_REPEATING = 101
+
+        private const val DATE_FORMAT = "yyyy-mm-dd"
+        private const val TIME_FORMAT = "HH:mm"
     }
 
     override fun onReceive(context: Context, intent: Intent) {
@@ -31,5 +40,42 @@ class AlarmReceiver : BroadcastReceiver() {
 
     private fun showToast(context: Context, title: String, message: String?){
         Toast.makeText(context,"$title : $message", Toast.LENGTH_LONG).show()
+    }
+
+    fun setOneTimeAlarm(context: Context, type: String, date: String, time: String, message:String){
+        if (isDateInvalid(date, DATE_FORMAT) || isDateInvalid(time, TIME_FORMAT)) return
+
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent (context, AlarmReceiver::class.java)
+        intent.putExtra(EXTRA_MESSAGE, message)
+        intent.putExtra(EXTRA_TYPE, type)
+
+        Log.e("ONE TIME", "$date $time")
+        val dateArray = date.split("-").toTypedArray()
+        val timeArray = date.split(":").toTypedArray()
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.YEAR, Integer.parseInt(dateArray[0]))
+        calendar.set(Calendar.MONTH, Integer.parseInt(dateArray[1])-1)
+        calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateArray[2]))
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
+        calendar.set(Calendar.SECOND,0)
+
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_ONETIME,intent,0)
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+
+        Toast.makeText(context, "One time alarm set up", Toast.LENGTH_LONG).show()
+    }
+
+    fun isDateInvalid(date: String, format:String): Boolean {
+        return try {
+            val df = SimpleDateFormat(format, Locale.getDefault())
+            df.isLenient = false
+            df.parse(date)
+            false
+        } catch (e: ParseException){
+            true
+        }
     }
 }
